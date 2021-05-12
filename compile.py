cc4af5c73e960f86614d2585a8588b5dd6e8be3f
Raghav jha <46911401+RAGHAVJHA01@users.noreply.github.com>
2021-05-12 14:53:23 +0530

    Create compile.py

---
+++ compile.py
@@ -0,0 +1,7 @@
+# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+#fitting and saving the model
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+print("model created")
